e374402b7aa497c67502c15c5ed70815bb888160
Elvis Basic <133909877+plavsko@users.noreply.github.com>
2024-05-23 23:12:33 -0400

    Erreur d'indenation

--- Exercice_1.py
+++ Exercice_1.py
@@ -9,7 +9,7 @@ with open('data.json', 'r') as fichier_json:
 with open('num_complexes.csv', 'w', newline='') as fichier_csv:
     writer = csv.writer(fichier_csv)
 
-writer.writerow(['reel', 'imaginaire'])
+    writer.writerow(['reel', 'imaginaire'])
 
-for num_complexe in data:
-    writer.writerow(num_complexe)
+    for num_complexe in data:
+        writer.writerow(num_complexe)
