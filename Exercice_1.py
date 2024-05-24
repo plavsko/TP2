3b26a5effc91262275c945f15f1ac5525e827840
Elvis Basic <133909877+plavsko@users.noreply.github.com>
2024-05-23 23:02:43 -0400

    Identation 2.0

--- Exercice_1.py
+++ Exercice_1.py
@@ -4,7 +4,7 @@ import json
 import csv
 
 with open('data.json', 'r') as fichier_json:
-    data = json.load(json_file)
+    data = json.load(fichier_json)
 
 with open('num_complexes.csv', 'w', newline='') as fichier_csv:
     writer = csv.writer(fichier_csv)
@@ -12,4 +12,4 @@ with open('num_complexes.csv', 'w', newline='') as fichier_csv:
 writer.writerow(['reel', 'imaginaire'])
 
 for num_complexe in data:
-        writer.writerow(num_complexe)
+    writer.writerow(num_complexe)
