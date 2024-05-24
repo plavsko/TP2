b8b0143119813aa912b9ff002dbf8a3ad58d8478
Abdellah <e2235763@cmaisonneuve.qc.ca>
2024-05-23 23:17:40 -0400

    code complet

--- Exercice_2.py
+++ Exercice_2.py
@@ -1,1 +1,17 @@
 import csv
+import csv
+def charger_pokemons_csv(filename):
+pokemons = {}
+with open(filename, 'r') as csvfile:
+reader = csv.reader(csvfile)
+for row in reader:
+name = row[0]
+stats = list(map(int, row[1:]))
+pokemons[name] = stats
+return pokemons
+pkmn = charger_pokemons_csv(pokemon.csv)
+for nom, stats in pkmn.items():
+print(f{nom}: {stats})
+print(isinstance(pkmn, dict))
+print(isinstance(pkmn[Pikachu], list))
+print(isinstance(pkmn[Pikachu][0], int))
