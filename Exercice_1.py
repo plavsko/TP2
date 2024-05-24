baa305585faa8105caa0033dea241f13077ffe66
ElvisBasic <e2357627@cmaisonnneuve.qc.ca>
2024-05-23 22:34:15 -0400

    Ouvrir le fichier csv

--- Exercice_1.py
+++ Exercice_1.py
@@ -4,3 +4,5 @@ import json
 import csv
 with open('data.json', 'r') as fichier_json
 data = json.load(json_file)
+with open('num_complexes.csv', 'w', newline='') as fichier_csv
+writer = csv.writer(fichier_csv)
