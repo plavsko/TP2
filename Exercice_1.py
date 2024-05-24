b2b396e37e995e0dcfa83f9672219291794d93f5
Elvis Basic <133909877+plavsko@users.noreply.github.com>
2024-05-23 22:47:10 -0400

    Update Exercice_1.py

--- Exercice_1.py
+++ Exercice_1.py
@@ -3,13 +3,7 @@
 import json
 import csv
 with open('data.json', 'r') as fichier_json
-<<<<<<< HEAD
 data = json.load(json_file)
 with open('num_complexes.csv', 'w', newline='') as fichier_csv
 writer = csv.writer(fichier_csv)
-=======
-data = json.load(fichier_json)
->>>>>>> 9bc0328f0dcc385eab2c61fcc64971ef752bf197
-writer = csv.writer(fichier_csv)
-with open('num_complexes.csv', 'w', newline='') as fichier_csv
-writer = csv.writer(fichier_csv)
+
